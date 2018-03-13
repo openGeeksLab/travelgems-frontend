deea7bc1f945be1fc7060ae6a6f4d3746df641cc
Panagiotis Triantafyllou <panost10@gmail.com>
2018-03-13 10:42:47 +0200

    Activities screen

--- src/containers/Activities/Activities.js
+++ src/containers/Activities/Activities.js
@@ -1,28 +1,76 @@
 import WarplyMobileSDK from '../../services/WarplyMobileSDK';
 import Config from 'react-native-config';
 import React, { Component } from "react";
-import { Image, View, StatusBar, Linking, TouchableOpacity } from "react-native";
+import { Image, View, StatusBar, Linking, TouchableOpacity, ImageBackground, ScrollView } from "react-native";
 import { List, ListItem, Avatar, Icon } from "react-native-elements"
 import { Container, H3, Text, Title, Body, Left, Right } from "native-base";
-import { Header,Button } from "react-native-elements";
+import { Header, Button } from "react-native-elements";
 import styles from "./styles";
 
 class Activities extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
     }
   }
   static navigationOptions = {
-    tabBarLabel:'Activities',
-    tabBarIcon:({tintColor}) =>(
+    tabBarLabel: 'Activities',
+    tabBarIcon: ({ tintColor }) => (
       <Icon name='home' type='home' color='#fff' />
     )
   }
   render() {
     return (
       <Container>
-            <Text>Activities!</Text>
+        <ImageBackground source={{ uri: 'http://via.placeholder.com/410x300' }}
+          style={styles.imageBackground} >
+          <View style={styles.imageText}>
+            <Text style={styles.imageTitle}>Jeep Safari in the{'\n'}National Park</Text>
+            <Text style={styles.imageSubtitle}>Kefalonia</Text>
+          </View>
+        </ImageBackground>
+        <ScrollView>
+          <View>
+            <View style={styles.gridRow}>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+            </View>
+            <View style={styles.gridRow}>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+              <View style={styles.gridItem}>
+                <Image source={{ uri: 'http://via.placeholder.com/31x31' }} style={styles.gridImage}></Image>
+                <Text style={styles.gridTitle}>TITLE</Text>
+                <Text style={styles.gridSubtitle}>Subtitle</Text>
+              </View>
+            </View>
+          </View>
+
+          <View>
+            <Text style={styles.infoViewTitle}>Info</Text>
+          </View>
+        </ScrollView>
       </Container>
     );
   }
