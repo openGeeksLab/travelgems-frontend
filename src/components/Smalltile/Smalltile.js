b6ff42248f67005eeece646b5317ff3b0968c6d2
Dimitris Koulouris <dimitrisk@warp.ly>
2018-03-13 14:26:08 +0200

    smallTile and grid first approach

---
+++ src/components/Smalltile/Smalltile.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import {View,Image} from "react-native";
+import { Tile } from "react-native-elements"
+import { Container, Header, Content, Thumbnail, Text, Title } from 'native-base';
+import styles from "./styles";
+import Icon from 'react-native-vector-icons/Entypo';
+
+class Smalltile extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+
+	render() {
+		return (
+      <View style={{flex:1,padding:10}}>
+        <Image
+            style={{flex:1, height: undefined, width: undefined,padding:0,marginTop:10}}
+           resizeMode="contain"
+          source={{uri: this.props.img}}
+        />
+        <Title style={{padding:0,textAlign:"left",color:"black"}}>{this.props.title}</Title>
+        <Text>{this.props.subtitle}</Text>
+      </View>
+		);
+	}
+}
+Smalltile.defaultProps = {img: 'https://facebook.github.io/react-native/docs/assets/favicon.png'};
+export default Smalltile;
--- src/containers/Discover/Discover.js
+++ src/containers/Discover/Discover.js
@@ -4,9 +4,12 @@ import React, { Component } from "react";
 import { Image, View, StatusBar, Linking, TouchableOpacity } from "react-native";
 import { List, ListItem, Avatar, Icon } from "react-native-elements"
 import { Container, H3, Text, Title, Body, Left, Right } from "native-base";
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import { Header,Button } from "react-native-elements";
 import styles from "./styles";
+import ParallaxScroll from '@monterosa/react-native-parallax-scroll';
 
+import Smalltile from '../../components/Smalltile/Smalltile'
 class Discover extends Component {
   constructor (props) {
     super(props)
@@ -22,7 +25,16 @@ class Discover extends Component {
   render() {
     return (
       <Container>
-            <Text>Discover!</Text>
+        <Grid>
+          <Row>
+            <Col><Smalltile title='Kos' subtitle='Greece' img='https://facebook.github.io/react-native/docs/assets/favicon.png'/></Col>
+            <Col><Smalltile title='Zakynthos' subtitle='Greece '/></Col>
+          </Row>
+          <Row>
+            <Col><Smalltile title='Crete' subtitle='Greece'/></Col>
+            <Col><Smalltile title='Meteora' subtitle='Greece'/></Col>
+          </Row>
+        </Grid>
       </Container>
     );
   }
