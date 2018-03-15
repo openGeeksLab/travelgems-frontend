dc10a411331085074c92a8628469b34c69f6d7c8
Dimitris Koulouris <dimitrisk@warp.ly>
2018-03-15 11:23:27 +0200

    some extras global styles

--- src/constants/Styles.js
+++ src/constants/Styles.js
@@ -1,1 +1,5 @@
 export const COLOR_TURQUOISE = '#46DFE8';
+export const COLOR_TURQUOISE_DARDK = '#15ABC2';
+export const COLOR_RED = '#FF295B';
+export const COLOR_GREY_LIGHT = '#A1A1A1';
+export const COLOR_BLACK = '#222222';
