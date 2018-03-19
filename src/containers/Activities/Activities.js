48826cee61837f8f7b40130d8d3b86de3bde3eb2
Dimitris Koulouris <dimitrisk@warp.ly>
2018-03-19 13:21:44 +0200

    wide tile component and activities screen

--- src/containers/Activities/Activities.js
+++ src/containers/Activities/Activities.js
@@ -1,11 +1,12 @@
 import Config from 'react-native-config';
 import React, { Component } from "react";
-import { Image, View, StatusBar, Linking, TouchableOpacity, ImageBackground, ScrollView } from "react-native";
-import { List, ListItem, Avatar, Icon } from "react-native-elements"
-import { Container, H3, Text, Title, Body, Left, Right } from "native-base";
+import { Image, View, StatusBar, Linking, TouchableOpacity,ScrollView } from "react-native";
+import { Icon } from "react-native-elements"
+import { Container, H3, Text, Title, Body, Left, Right,List, ListItem} from "native-base";
 import { Header, Button } from "react-native-elements";
 import Backgroundimage from '../../components/Backgroundimage/Backgroundimage';
 import styles from "./styles";
+import Widetile from '../../components/Widetile/Widetile';
 
 const gridImage = 'https://www.olympicholidays.com/media/20570/fiskardo_kefalonia_greece.jpg?center=0.37948717948717947,0.51&mode=crop&quality=70&width=550&height=358&rnd=131302560700000000';
 
@@ -31,7 +32,16 @@ class Activities extends Component {
         centerComponent={<Title>travelgems</Title>}
         rightComponent={<Icon name='home' type='home' color='#fff' onPress={() => this.props.navigation.navigate("Home")} /> }
       />
-            <Text >Activities</Text>
+      <ScrollView style={{}}>
+        <List>
+          <ListItem style={{margin:0,padding:0,marginLeft:0,paddingLeft:15,paddingRight:15,borderBottomWidth:0}}>
+            <Widetile title="title" subtitle="subtitle" img="https://www.olympicholidays.com/media/20570/fiskardo_kefalonia_greece.jpg?center=0.37948717948717947,0.51&mode=crop&quality=70&width=550&height=358&rnd=131302560700000000"/>
+          </ListItem>
+          <ListItem style={{margin:0,padding:0,marginLeft:0,paddingLeft:15,paddingRight:15,borderBottomWidth:0}}>
+            <Widetile title="title" subtitle="subtitle" img="https://www.olympicholidays.com/media/20570/fiskardo_kefalonia_greece.jpg?center=0.37948717948717947,0.51&mode=crop&quality=70&width=550&height=358&rnd=131302560700000000"/>
+          </ListItem>
+        </List>
+      </ScrollView>
       </Container>
     );
   }
