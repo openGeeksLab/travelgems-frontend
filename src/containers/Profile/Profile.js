029b2e6830fe1799784f9dd01d85f86509a9547f
Panagiotis Triantafyllou <panost10@gmail.com>
2018-04-05 17:05:24 +0300

    Profile screen with data (temp)

--- src/containers/Profile/Profile.js
+++ src/containers/Profile/Profile.js
@@ -1,30 +1,122 @@
 import Config from 'react-native-config';
 import React, { Component } from "react";
-import { Image, View, StatusBar, Linking, TouchableOpacity } from "react-native";
+import { Image, View, StatusBar, Linking, TouchableOpacity, ScrollView } from "react-native";
 import { List, ListItem, Avatar, Icon } from "react-native-elements"
 import { Container, H3, Text, Title, Body, Left, Right } from "native-base";
-import { Header,Button } from "react-native-elements";
+import { Header, Button } from "react-native-elements";
 import styles from "./styles";
+import HorizontalScroll from '../../components/HorizontalScroll/HorizontalScroll';
+import Smalltile from '../../components/Smalltile/Smalltile';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { show } from 'redux-modal';
 
 class Profile extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
+
+    let favouriteDestinations = this.props.favouriteDestinations;
+    let profileBadges = this.props.profileBadges;
+    let profileInfo = this.props.profileInfo;
+    let firstName = this.props.profileInfo["firstname"];
+    let lastName = this.props.profileInfo["lastname"];
+    let displayName = this.props.profileInfo["display_name"];
+    let imageUrl = this.props.profileInfo["image_url"];
+    // let imageUrl = 'http://via.placeholder.com/92x92';
+    let profileImage;
+    let fullName;
+
+    if (imageUrl)
+      profileImage = [{ uri: imageUrl }];
+    else
+      profileImage = require('../../assets/images/Profile/defaultProfilePhoto.png');
+
+    if (!firstName && !lastName)
+      fullName = displayName;
+    else if (firstName && !lastName)
+      fullName = firstName;
+    else if (!firstName && lastName)
+      fullName = lastName;
+    else
+      fullName = firstName + " " + lastName;
+
     this.state = {
+      lines: [],
+      favouriteDestinations: favouriteDestinations,
+      profileBadges: profileBadges,
+      profileInfo: [],
+      fullName: fullName,
+      profileImage: profileImage
     }
+    // debugger;
   }
   static navigationOptions = {
-    tabBarLabel:'Profile',
-    tabBarIcon:({tintColor}) =>(
+    tabBarLabel: 'Profile',
+    tabBarIcon: ({ tintColor }) => (
       <Icon name='person' type='home' color='#fff' />
     )
   }
+
   render() {
     return (
       <Container>
-            <Text>Profile!</Text>
+        <View style={styles.profileInfo}>
+          <Image style={styles.profileImage} source={this.state.profileImage}></Image>
+          <Text style={styles.profileText}>{this.state.fullName}</Text>
+          <TouchableOpacity style={styles.profileButton}>
+            <Text style={styles.textButton}>Edit Travel Profile</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.profileCategories}>
+          {
+            this.state.profileBadges.map((i) => (
+              <View style={styles.categoriesImageInfo}>
+                <Image style={styles.categoriesImage} source={{ uri: 'http://via.placeholder.com/57x57' }}></Image>
+                <Text style={styles.categoriesText}>{i}</Text>
+              </View>
+            ))
+          }
+          {/* <View style={styles.categoriesImageInfo}>
+            <Image style={styles.categoriesImage} source={{ uri: 'http://via.placeholder.com/57x57' }}></Image>
+            <Text style={styles.categoriesText}>PUBLIC</Text>
+            <Text style={styles.categoriesText}>TRANSPORTATION</Text>
+          </View>
+          <View style={styles.categoriesImageInfo}>
+            <Image style={styles.categoriesImage} source={{ uri: 'http://via.placeholder.com/57x57' }}></Image>
+            <Text style={styles.categoriesText}>CHRISTMAS</Text>
+            <Text style={styles.categoriesText}>HOLIDAYS</Text>
+          </View> */}
+        </View>
+
+        <View style={styles.wishlist}>
+          <View style={styles.wishlistHeaders}>
+            <Title style={styles.title}>Wishlist</Title>
+            <Text style={styles.titleButton}>View All</Text>
+          </View>
+          <View>
+            <ScrollView horizontal style={styles.horizontalScroll}>
+              {
+                this.state.favouriteDestinations.map((item, j) => (
+                  <View style={{ width: 150, height: 150 }}>
+                    <Smalltile title={item.name} subtitle={item.country} img={item.img_preview} />
+                  </View>
+                ))
+              }
+            </ScrollView>
+          </View>
+        </View>
       </Container>
     );
   }
 }
 
-export default Profile;
+// export default Profile;
+export default connect(function (state) {
+  return {
+    favouriteDestinations: state.profile.favouriteDestinations,
+    profileBadges: state.profile.profileBadges,
+    profileInfo: state.profile.profileInfo
+  }
+},
+  dispatch => bindActionCreators({ show }, dispatch))(Profile)
--- src/containers/Profile/styles.js
+++ src/containers/Profile/styles.js
@@ -3,10 +3,11 @@ const React = require("react-native");
 const { StyleSheet, Dimensions, Platform } = React;
 
 const deviceHeight = Dimensions.get("window").height;
+const deviceWidth = Dimensions.get("window").width;
 
 export default {
   content: {
-    flex: 1
+    flex: 1,
   },
   imageContainer: {
     position: "absolute",
@@ -27,5 +28,90 @@ export default {
     color: "#D8D8D8",
     bottom: 6,
     marginTop: 5
+  },
+  profileInfo: {
+    height: deviceHeight / 3,
+    width: deviceWidth,
+    backgroundColor: "#041DB2",
+    flexDirection:"column",
+    alignItems: 'center',
+    paddingTop: 10
+  },
+  profileImage: {
+    height: 95,
+    width: 95,
+    borderWidth: 4,
+    borderRadius: 100,
+    borderColor: "#FFFFFF",
+  },
+  profileText: {
+    color: "#FFFFFF",
+    fontSize: 18,
+    padding: 10,
+    fontWeight: "bold",
+  },
+  profileButton: {
+    backgroundColor: "#46DFE8",
+    borderRadius: 2,
+    width: 150,
+    height: 30,
+    padding: 5,
+    elevation: 10,
+    outlineProvider: 'bounds'
+  },
+  textButton: {
+    color: "#FFFFFF",
+    alignSelf: "center",
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+  profileCategories: {
+    width: deviceWidth,
+    height: deviceHeight / 6,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: 'space-between',
+    paddingLeft: 32,
+    paddingRight: 32,
+    paddingTop: 10
+  },
+  categoriesImageInfo: {
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  categoriesImage: {
+    width: 57,
+    height: 57,
+    borderRadius: 100,
+  },
+  categoriesText: {
+    fontSize: 10,
+    color: "#222222",
+    opacity: 0.5,
+  },
+  title: {
+    color: '#222222',
+    textAlign: "left",
+    fontSize: 24,
+  },
+  wishlist: {
+    flexDirection: "column",
+    paddingLeft: 32,
+    paddingRight: 32,
+    paddingTop: 10,
+  },
+  titleButton: {
+    color: '#46DFE8',
+    fontSize: 14,
+    textAlign: "right"
+  },
+  horizontalScroll: {
+    height: 150,
+    width: deviceWidth
+  },
+  wishlistHeaders: {
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems: "center"
   }
 };
