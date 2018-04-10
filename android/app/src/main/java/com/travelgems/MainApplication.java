ec857bb114a02afaeba0f1e4ea1b7737b2f375bd
Stratakis Nikolaos <nstratak@gmail.com>
2018-04-10 16:02:51 +0300

    correct package name

--- android/app/src/main/java/com/travelgems/MainApplication.java
+++ android/app/src/main/java/com/travelgems/MainApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.travelgems;
 
 import android.app.Application;
 
--- src/components/MyOrderTab/MyOrderTab.js
+++ src/components/MyOrderTab/MyOrderTab.js
@@ -37,7 +37,7 @@ const MyOrderTab = TabNavigator({
     },
 );
 MyOrderTab.navigationOptions = {
-    title: 'tab example',
+    title: 'tab travelgems',
 };
 
 export default MyOrderTab;
